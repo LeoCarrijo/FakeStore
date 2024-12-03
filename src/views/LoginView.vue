018c56348d84335e97d3e300868fcd8a240d3f6e
LeoCarrijo <lh83712@gmail.com>
2024-12-03 15:06:05 -0300

    :sparkles: feat: Criação da página de login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Login</h1>
+  <form class="form__login" @submit.prevent="handleSubmit()">
+    <div class="input__container">
+      <label for="username">Usuário: </label>
+      <input type="text" name="username" id="username" />
+    </div>
+    <div class="input__container">
+      <label for="password">Senha: </label>
+      <input type="password" name="password" id="password" />
+    </div>
+    <input type="submit" />
+    <input type="reset" />
+  </form>
+</template>
+
+<script setup>
+function handleSubmit() {
+  console.log('Formulário enviado')
+}
+</script>
+
+<style scoped></style>
