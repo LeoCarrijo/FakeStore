7e2ff52545315614c66660ba696f4c5d11c9835c
LeoCarrijo <lh83712@gmail.com>
2024-12-03 17:18:15 -0300

    :sparkles: feat: Criação da página de produtos

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Produtos</h1>
+  <ul>
+    <li v-for="product in products" :key="product.id">
+      {{ product.title }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import store from '@/store'
+import { computed } from 'vue'
+
+const products = computed(() => store.state.products)
+</script>
+
+<style scoped></style>
